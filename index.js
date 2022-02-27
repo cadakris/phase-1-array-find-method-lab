7a6f51ab06216578ccaf1312613cd0b267fec226
Kristen <kriscadacio@gmail.com>
2022-02-27 07:51:26 -0500

    commit tree 443a8a3231ae5cf7505fbcb38bc5f454720ad11c

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // code your solution here
+// function superbowlWin (arr) {
+//    const found = arr.find(element => element.result === "W")
+//     return found && found.year
+// }
+
+
+// write a function called SuperbowlWin() that takes in 1 argument an array of objects
+// each object has a year and a result
+// use find to test each object to see if result is"W"
+// if it is a win return the year
+// if not a win return undefined
+
+const superbowlWin = (arrayOfObj) => {
+    const foundRecord = arrayOfObj.find(record => record.result === "W");
+        return (foundRecord ? foundRecord.year : undefined);
+}
+
+// function superbowlWin (arrayOfObj) {
+//     const foundRecord = arrayOfObj.find(record => record.result === "W");
+//         return (foundRecord ? foundRecord.year : undefined);
+// }
